f60244fa8a03d8837e900a991f68a712bf190370
Orwiss <pjtpjt159@naver.com>
2024-04-18 17:51:07 +0900

    Component Test

--- test/src/App.js
+++ test/src/App.js
@@ -3,21 +3,9 @@ import React, {Component} from 'react'
 export default class App extends Component {
   render() {
     return (
-      <main className="main-container">
-        <h1>예산 계산기</h1>
-
-        <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-          {
-
-          }
-        </div>
-
-        <div style={{ display: 'flex', justifyContent: 'end', marginTop: '1rem' }}>
-          <p style={{ fontSize: '2rem' }}>
-            총 지출: <span>원</span>
-          </p>
-        </div>
-      </main>
+      <div className="main-container">
+        <h1>Hello!</h1>
+      </div>
     )
   }
 }
--- test/src/index.js
+++ test/src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Test1 from './test';
 import reportWebVitals from './reportWebVitals';
+
+function Test2() {
+  const style = {
+    backgroundColor: 'gray',
+    color: 'white'
+  }
+
+  return (
+    <div className='content' style={style}>
+        <h2>Function Test</h2>
+    </div>
+  )
+}
+export default Test2;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
+    <App/>
+    <App/>
+    <Test1/>
+    <Test2/>
   </React.StrictMode>
 );
 
