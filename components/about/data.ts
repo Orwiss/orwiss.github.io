4615cfd71ac8c69072864b47c36c12150f533e2a
Orwiss <7181108+Orwiss@users.noreply.github.com>
2025-06-20 16:37:16 +0900

    Update About Data

--- components/about/data.ts
+++ components/about/data.ts
@@ -11,6 +11,10 @@ export const educationData = [
 
 export const exhibitionData = [
   {
+    date: "2025",
+    title: "Pizza Saver Office 공간대여 팀 전시 <헤스티아의 화로 위에서>",
+  },
+  {
     date: "2024",
     title: "서울디자인페스티벌 디자인컨버전스학부 전시\n<온실(On-Sil)>",
   },
@@ -25,6 +29,14 @@ export const exhibitionData = [
 ];
 
 export const projectData = [
+  {
+    date: "2025",
+    title: "국립현대미술관 창동레지던시 입주연구자 기획전시 <무인공장> 작품 협력",
+  },
+  {
+    date: "2025",
+    title: "국립현대미술관 창동레지던시 입주작가전 <공명의 숲> 작품 협력",
+  },
   {
     date: "2024",
     title: "하동군 '하동시장 아이조아 노리터' 인터랙티브 콘텐츠 개발",
